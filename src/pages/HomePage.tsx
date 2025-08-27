c38fb229e6288d30bba2ea320ba4fe00e49aa13c
EY2-SHA <shankomengesha@gmail.com>
2025-08-27 10:55:23 +0300

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -85,7 +85,7 @@ const HomePage: React.FC = () => {
   ];
 
   return (
-    <div className="pt-26">
+    <div className="pt-16">
      {/* Hero Section */}
 <section className="relative min-h-screen flex items-center justify-center overflow-hidden">
   {/* Background Image */}
